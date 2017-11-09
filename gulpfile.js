d5630ca23a031fe37fad4b603261c98171c212ba
Toyrone <akinstoyosi@gmail.com>
2017-11-10 00:36:20 +0100

    Set up dev environment

---
+++ gulpfile.js
@@ -0,0 +1,14 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var concat = require('gulp-concat');
+var browserSync = require('browser-sync').create();
+
+gulp.task('sass', function() {
+  gulp.src('template/assets/scss/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('template/assets/css'));
+});
+
+gulp.task('default', function() {
+  gulp.watch( 'template/assets/scss/**/*.scss', ['sass'] );
+});
