ac31930c57799cf0f3af7da225b72dbcc5e84a9f
Toyrone <akinstoyosi@gmail.com>
2017-11-17 18:29:12 +0100

    Add link to home page

--- gulpfile.js
+++ gulpfile.js
@@ -3,13 +3,29 @@ var sass = require('gulp-sass');
 var concat = require('gulp-concat');
 var browserSync = require('browser-sync').create();
 
+
+gulp.task('browserSync', function() {
+  browserSync.init({
+    server: {
+      baseDir: 'template'
+    }
+  })
+})
+
 gulp.task('sass', function() {
   gulp.src('template/assets/scss/**/*.scss')
     .pipe(concat('main.scss'))
     .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('template/assets/css'));
+    .pipe(gulp.dest('template/assets/css'))
+    .pipe(browserSync.reload({
+      stream: true
+    }))
 });
 
-gulp.task('default', function() {
+
+
+gulp.task('default', ['browserSync', 'sass'], function() {
   gulp.watch( 'template/assets/scss/**/*.scss', ['sass'] );
+  gulp.watch('template/assets/*.html', browserSync.reload);
+  gulp.watch('template/assets/js/**/*.js', browserSync.reload);
 });
